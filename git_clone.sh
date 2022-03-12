fd38dac315ef64625d96fd4d198128485ee06d1d
Andreas Kampert <andreas@kampert.de>
2022-03-12 19:20:11 +0100

    Git Clone

--- git_clone.sh
+++ git_clone.sh
@@ -1,14 +1,19 @@
-############
+#######################################
 #
 # Github Clone für Admin und Client/Server
 #
-#################
+#######################################
 
-echo '   Clone Github'
+echo '**************************************************'
+echo '   Github Clone für Admin und Client/Server'
+echo ' '
+echo '**************************************************'
+
 cd $home
 
-rm -r git_vialinked_admin
-rm -r git_vialinked
+sudo rm -r git_vialinked_admin
+sudo rm -r git_vialinked
 
 git clone git@github.com:kampert/vialinked_admin.git  git_vialinked_admin
 git clone git@github.com:FriedhelmKoch/viaLinked.git  git_vialinke
+
