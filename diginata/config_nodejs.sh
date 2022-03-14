dc8c95502e69f08bf353b7b89e207393ac4c0e41
Andreas Kampert <59868243+kampert@users.noreply.github.com>
2022-03-14 19:13:22 +0100

    Update config_nodejs.sh

--- diginata/config_nodejs.sh
+++ diginata/config_nodejs.sh
@@ -7,7 +7,11 @@ sudo chmod -R 755 /opt/diginata
 sudo scp -r root@h2911291.stratoserver.net:"/var/www/vhosts/vialinked.com/*" /opt/diginata
 cd /opt/diginata
 ls
-
+sudo cp -r /opt/diginata/api-server/server/ /opt/diginata/server/
+sudo cp /opt/diginata/api-server/routes.js /opt/diginata/routes.js
+# sudo cp /opt/diginata/api-server/routes.js /opt/diginata/server/routes.js
+cd /opt/diginata
+sudo rm -r /opt/diginata/api-server
 
 sudo chown -R $USER:$USER /opt/diginata
 sudo chmod -R 755 /opt/diginata
