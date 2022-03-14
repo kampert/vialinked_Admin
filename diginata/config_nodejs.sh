8a48872654e01054313f91858a8ee75870f9b8d0
Andreas Kampert <59868243+kampert@users.noreply.github.com>
2022-03-14 19:13:54 +0100

    Update config_nodejs.sh

--- diginata/config_nodejs.sh
+++ diginata/config_nodejs.sh
@@ -13,7 +13,7 @@ sudo cp /opt/diginata/api-server/routes.js /opt/diginata/routes.js
 cd /opt/diginata
 sudo rm -r /opt/diginata/api-server
 
-sudo chown -R $USER:$USER /opt/diginata
+sudo chown -R $USER:$GROUP /opt/diginata
 sudo chmod -R 755 /opt/diginata
 cd /opt/diginata/api-server/server
 cat package.json 
