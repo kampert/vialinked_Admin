0b90d7c6040db3aaa3d87cb77a553c07bc597c78
Andreas Kampert <59868243+kampert@users.noreply.github.com>
2022-03-17 20:38:14 +0100

    Create deploy_server.sh

---
+++ production/deploy_server.sh
@@ -0,0 +1,11 @@
+sudo cp -r /home/viaadmin/git_vialinked/server /var/www/vhosts/vialinked.com/api-server/server
+sudo cp /home/viaadmin/git_vialinked/routes.js /var/www/vhosts/vialinked.com/routes.js
+sudo cp /home/viaadmin/git_vialinked/routes.js /var/www/vhosts/vialinked.com/api-server/routes.js
+# server.js als Executable markieren
+cd /var/www/vhosts/vialinked.com/api-server/server/src
+sudo chmod +x server.js
+# .env File korrigieren
+cd /var/www/vhosts/vialinked.com/api-server/server
+sudo cp /home/viaadmin/git_vialinked_admin/procution/server_env /var/www/vhosts/vialinked.com/api-server/server/.env
+#
+sudo npm install
