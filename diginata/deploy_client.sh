e6cf0eb385e81e446456080cb08f59b545b6a9e2
Andreas Kampert <andreas@kampert.de>
2022-03-21 18:14:05 +0100

    1

--- diginata/deploy_client.sh
+++ diginata/deploy_client.sh
@@ -1,29 +1,86 @@
-############
+#!/bin/sh
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+   deploy_client.sh
+#%
+#% DESCRIPTION
+#%    
+#%    Sichert den alten Zustand des Clients und kopiert anschliessend die 
+#%    Client Sourcen auf dem Server in die richtigen Verzeichnisse.
+#%    Auf github liegen bereits transpilierte Sourcen, daher ist eine Transpilierung nicht 
+#%    nötig.
+#%    nginx wird neu gestartet
+#%
+#% Voraussetzung: git_clone.sh wurde ausgeführt.
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version        deploy_client.sh 1.0
+#-    author          Andreas Kampert
+#-
+#================================================================
+#  HISTORY
+#     2022_03-21 : Andreas Kampert : Script creation
 #
-# Aufbau des Clients
-#
-# Voraussetzung: git_clone.sh wurde durchgeführt.
-#
-############
+#================================================================
+# END_OF_HEADER
+#================================================================
 
+
+######################################################################
+#
 # Schritt 1: Transpile client
-cd ~/git_vialinked/client
-rm .env
-cp ~/git_vialinked_admin/diginata/nginx_env .env
-sudo npm install
-sudo npm run build-dev
+# Dieser Schritt entfällt, da Friedhelm jetzt in der Entwicklungsumgebung transpiliert und 
+# das Verzeichnis httpdocs zur Verfügung stellt
+#
+######################################################################
+# cd ~/git_vialinked/client
+# rm .env
+# cp ~/git_vialinked_admin/diginata/nginx_env .env
+# sudo npm install
+# sudo npm run build-dev
 
+
+######################################################################
+#
 # Schritt 2: Sichern der alten Version
+# Die alte Version des Clients wird mit Datum und Zeitstempel gesichert, um 
+# ein schnelles Restore möglich zu machen
+#
+######################################################################
 datum=`date +%Y-%m-%d_%H-%M`
-echo $datum
-sudo mv /var/www/diginata /var/www/diginata.$datum
+echo '   '
+echo '**************************************************'
+echo 'Alte Client Version wird gesichert nach -> /var/www/diginata.'$datum
 
+sudo mv -v /var/www/vialinked /var/www/diginata.$datum
+echo '   '
+
+######################################################################
+#
 # Schritt 3: Kopieren der neuen Version
-ls /var/www
-sudo mv ~/git_vialinked/client/www /var/www/diginata
+#
+######################################################################
+echo '**************************************************'
+echo 'Neue Version wird kopiert'
+sudo mkdir /var/www/diginata
+sudo cp -r -v ~/git_vialinked/client/httpdocs/* /var/www/diginata
+echo '   '
 
-# Schritt 4: Kontrolle und Neustart
-ls /var/www
-sudo nginx -t
+######################################################################
+#
+# Schritt 4: Neustart des nginx - Server
+#
+######################################################################
+echo '**************************************************'
+echo ' Neustart nginx  '
+sudo nginx -t                                  # Kontrolle der nginx - Konfiguration
 sudo systemctl restart nginx
+echo '   '
+echo '*** Ende deploy_client.sh ***'
+echo '   '
+
+
 
--- production/deploy_server.sh
+++ production/deploy_server.sh
@@ -1,11 +1,106 @@
-sudo cp -r /home/viaadmin/git_vialinked/server/* /var/www/vhosts/vialinked.com/api-server/server
-sudo cp /home/viaadmin/git_vialinked/routes.js /var/www/vhosts/vialinked.com/routes.js
-sudo cp /home/viaadmin/git_vialinked/routes.js /var/www/vhosts/vialinked.com/api-server/routes.js
+#!/bin/sh
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+   deploy_server.sh
+#%
+#% DESCRIPTION
+#%    
+#%    Sichert den alten Zustand des Servers und kopiert anschliessend die 
+#%    Server Sourcen auf dem Server in die richtigen Verzeichnisse.
+#%    Zusätzlich ist ein npm install notwendig. 
+#%    
+#%    nodejs wird neu geladen und gestartet
+#%
+#% Voraussetzung: git_clone.sh wurde ausgeführt.
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version        deploy_server.sh 1.0
+#-    author          Andreas Kampert
+#-
+#================================================================
+#  HISTORY
+#     2022_03-21 : Andreas Kampert : Script creation
+#
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+
+######################################################################
+#
+# Schritt 1: Sichern der alten Version
+# Die alte Version des Servers wird mit Datum und Zeitstempel gesichert, um 
+# ein schnelles Restore möglich zu machen
+#
+######################################################################
+datum=`date +%Y-%m-%d_%H-%M`
+echo '   '
+echo '**************************************************'
+echo 'Alte Server Version wird gesichert nach -> /opt/vialinked.'$datum
+
+# sudo mv -v /opt/vialinked /opt/vialinked.$datum
+sudo mv -v /var/www/vhosts/vialinked.com/api-server /var/www/vhosts/vialinked.com/api-server.$datum
+echo '   '
+
+######################################################################
+#
+# Schritt 2: Kopieren der neuen Version
+#
+######################################################################
+echo '**************************************************'
+echo 'Neue Version wird kopiert'
+sudo mkdir /var/www/vhosts/vialinked.com/api-server
+# sudo mkdir /opt/vialinked
+sudo cp -r -v ~/git_vialinked/server/*  /var/www/vhosts/vialinked.com/api-server/server
+sudo cp -v    ~/git_vialinked/routes.js /var/www/vhosts/vialinked.com/routes.js
+sudo cp -v    ~/git_vialinked/routes.js /var/www/vhosts/vialinked.com/api-server/routes.js
+echo '   '
+
+######################################################################
+#
+# Schritt 3: Setzen der richtigen Berechtigungen
+#
+######################################################################
+echo '**************************************************'
+echo 'Berechtigungen setzen'
 # server.js als Executable markieren
 cd /var/www/vhosts/vialinked.com/api-server/server/src
 sudo chmod +x server.js
+echo '   '
+
+######################################################################
+#
+# Schritt 4: Installation
+#
+######################################################################
+echo '**************************************************'
+echo 'npm install'
 # .env File korrigieren
 cd /var/www/vhosts/vialinked.com/api-server/server
-sudo cp /home/viaadmin/git_vialinked_admin/production/server_env /var/www/vhosts/vialinked.com/api-server/server/.env
-#
+sudo cp -v ~/git_vialinked_admin/production/server_env /var/www/vhosts/vialinked.com/api-server/server/.env
 sudo npm install
+echo '   '
+
+######################################################################
+#
+# Schritt 5: Neustart des nodejs - Servers
+#
+######################################################################
+echo '**************************************************'
+echo ' Neustart nodejs  '
+
+sudo sudo systemctl start vialinked-service
+echo '   '
+echo 'Zeige, ob Prozess gestartet ist'
+ps -ax | grep node
+
+echo 'Bei Fehlersuche'
+echo ' 1. sudo journalctl -u vialinked-service -n 20'
+echo ' 2. sudo sudo systemctl stop vialinked-service'
+echo ' 3. nodemon src/server.js'
+echo '   '
+echo '*** Ende deploy_server.sh ***'
+echo '   '
