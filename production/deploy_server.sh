3dc050addbf1deee35407ebcf181158d581ae2ce
Andreas Kampert <andreas@kampert.de>
2022-03-21 18:30:35 +0100

    1

--- production/deploy_server.sh
+++ production/deploy_server.sh
@@ -83,11 +83,12 @@ echo 'npm install'
 cd /var/www/vhosts/vialinked.com/api-server/server
 sudo cp -v ~/git_vialinked_admin/production/server_env /var/www/vhosts/vialinked.com/api-server/server/.env
 #
-sudo rm /var/www/vhosts/vialinked.com/api-server/server/package-lock.json
+# sudo rm /var/www/vhosts/vialinked.com/api-server/server/package-lock.json
 sudo rm /var/www/vhosts/vialinked.com/api-server/server/README.md
 sudo rm /var/www/vhosts/vialinked.com/api-server/server/docker-compose.yml
 #
-sudo npm install
+sudo npm install --no-fund
+sudo npm audit fix --force 
 echo '   '
 
 ######################################################################
