63e34ba32494513083187de9d2bda14fb0b5347e
Andreas Kampert <andreas@kampert.de>
2022-05-20 18:15:29 +0200

    1

--- diginata/config_apache.sh
+++ diginata/config_apache.sh
@@ -17,27 +17,26 @@ rm .env
 rm package-lock.json
 rm -r httpdocs
 rm README.md
+rm robots.txt
 cp ~/git_vialinked_admin/diginata/apache_env .env
 # Transpilieren verlangt die Installation von npm
 bash ~/git_vialinked_admin/diginata/install_nodejs.sh
 sudo npm install
+sudo npm install --force
 sudo npm run build-dev
-sudo mv ~/git_vialinked/client/www/ /var/www/diginata
-sudo nginx -t
-sudo systemctl restart nginx
+sudo mv -v ~/git_vialinked/client/www/ /var/www/diginata
+sudo apache2ctl configtest
+sudo systemctl restart apache2
 
 # Schritt 4: Bereitstellung und Definition Reverse Proxy
-sudo cp ~/git_vialinked_admin/diginata/config_nginx_diginata.com.conf /etc/nginx/sites-available/diginata.com.conf
-cat /etc/nginx/sites-available/diginata.com.conf 
-sudo ln -s /etc/nginx/sites-available/diginata.com.conf /etc/nginx/sites-enabled/
-
-# evtl fehlt hash_bucket
-# sudo nano /etc/nginx/nginx.conf
-sudo nginx -t
-sudo systemctl restart nginx
+sudo cp ~/git_vialinked_admin/diginata/config_apache_diginata.com.conf /etc/apache2/sites-available/diginata.com.conf
+cat /etc/apache2/sites-available/diginata.com.conf 
+sudo a2ensite diginata.conf
+sudo systemctl reload apache2
+sudo apache2ctl configtest
+sudo systemctl restart apache2
 
 # Schritt 5: HTTPS Konfiguration
-
 sudo snap install --classic certbot
 sudo apt install certbot python3-certbot-nginx
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
